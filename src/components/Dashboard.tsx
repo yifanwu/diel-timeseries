8e5c6552496d841fe7910616cba79b2b76ec56dc
yifanwu <yifan1030@gmail.com>
2019-06-21 16:23:44 -0700

    partially working

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import { DielComponent, ChartType, DielComponentProps, ChartSpec, ChannelName, UserSelection  } from "diel-ui";
 import { RangeUnitSelection, AnnotationStyle } from "diel-ui/build/types";
+import { diel } from "../setup";
 
 enum ComponentRelations {
   pack_ther = "pack_ther",
   pack_cell = "pack_cell",
+  gps = "gps",
+  speed = "speed",
+  // messages = "messages",
+  pack_break_regen = "pack_break_regen",
   current_time_selection_pretty = "current_time_selection_pretty"
 }
 
@@ -13,10 +18,11 @@ export default class Dashboard extends DielComponent<DielComponentProps> {
     super(props);
     this.state = {};
     this.BindDielOutputs(Object.keys(ComponentRelations));
+    diel.NewInput("time_selection", {minTs: null, maxTs: null});
   }
   render() {
     const {diel} = this.props;
-    const spec: ChartSpec = {
+    const multiSeriesSpec: ChartSpec = {
       chartType: ChartType.LineChart,
       channelByColumn: new Map([
         [ChannelName.x, "ts"],
@@ -27,7 +33,18 @@ export default class Dashboard extends DielComponent<DielComponentProps> {
         columns: ["time"],
         style: AnnotationStyle.Popup,
       }
-    }
+    };
+    const singleValueSpec: ChartSpec = {
+      chartType: ChartType.LineChart,
+      channelByColumn: new Map([
+        [ChannelName.x, "ts"],
+        [ChannelName.y, "val"],
+      ]),
+      annotation: {
+        columns: ["time"],
+        style: AnnotationStyle.Popup,
+      }
+    };
     const selectionHandler = (sel: UserSelection) => {
       // we know that this is a RangeUnitSelection
       const rangeSel = sel[0] as RangeUnitSelection;
@@ -38,8 +55,16 @@ export default class Dashboard extends DielComponent<DielComponentProps> {
       selectionHandler,
       deSelectHandler
     };
-    const therDiv = this.GenerateChart(spec, ComponentRelations.pack_ther, handler);
-    const cellDiv = this.GenerateChart(spec, ComponentRelations.pack_cell, handler);
+    const therDiv = this.GenerateChart(multiSeriesSpec, ComponentRelations.pack_ther, handler);
+    const cellDiv = this.GenerateChart(multiSeriesSpec, ComponentRelations.pack_cell, handler);
+    const gpsDiv = this.GenerateChart(multiSeriesSpec, ComponentRelations.gps, handler);
+    const speedDiv = this.GenerateChart(multiSeriesSpec, ComponentRelations.speed, handler);
+    const breakDiv = this.GenerateChart(singleValueSpec, ComponentRelations.pack_break_regen, handler);
+    // const rawMessages = this.state[ComponentRelations.messages]
+    // const messagesDiv = rawMessages
+    //   ? rawMessages.map(d => d["message"]).join("\n")
+    //   : null
+    //   ;
     const currentSelection = this.state[ComponentRelations.current_time_selection_pretty];
     const currentSelectionDiv = (currentSelection && currentSelection.length > 0)
       ? <h2>You have selected time range from {currentSelection[0]["minTs"]} to {currentSelection[0]["maxTs"]}</h2>
@@ -47,9 +72,18 @@ export default class Dashboard extends DielComponent<DielComponentProps> {
       ;
     return <>
       {currentSelectionDiv}
+      <h3>pack_ther</h3>
       {therDiv}
-      <br></br>
+      <h3>pack_cell</h3>
       {cellDiv}
+      <h3>gps</h3>
+      {gpsDiv}
+      <h3>speed</h3>
+      {speedDiv}
+      <h3>break</h3>
+      {breakDiv}
+      {/* <h3>Mesages</h3>
+      <p>{messagesDiv}</p> */}
     </>;
   }
 }
